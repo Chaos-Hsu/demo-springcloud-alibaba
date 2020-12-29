4d9fcced4b43958af7cf52caf87e075bc5efa837
xqc <xqc>
2020-12-29 11:13:12 +0800

    task:sentinel

--- demosa-shop-order/src/main/java/com/demosa/config/FilterConfig.java
+++ demosa-shop-order/src/main/java/com/demosa/config/FilterConfig.java
@@ -15,14 +15,14 @@ public class FilterConfig {
 
 
     //@Bean
-    public FilterRegistrationBean sentinelFilterRegist() {
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new CommonFilter());
-        registrationBean.setName("sentinelFilter");
-        registrationBean.setOrder(1);
-        registrationBean.addUrlPatterns("/*");
-        registrationBean.addInitParameter(CommonFilter.WEB_CONTEXT_UNIFY, "false");
-        return registrationBean;
-    }
+    //public FilterRegistrationBean sentinelFilterRegist() {
+    //    FilterRegistrationBean registrationBean = new FilterRegistrationBean<>();
+    //    registrationBean.setFilter(new CommonFilter());
+    //    registrationBean.setName("sentinelFilter");
+    //    registrationBean.setOrder(1);
+    //    registrationBean.addUrlPatterns("/*");
+    //    registrationBean.addInitParameter(CommonFilter.WEB_CONTEXT_UNIFY, "false");
+    //    return registrationBean;
+    //}
 
 }
