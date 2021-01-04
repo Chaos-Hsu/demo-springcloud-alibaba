cc7eff9686ad3c9836b73145f403c4c768ad249d
xqc <xqc>
2021-01-04 17:52:24 +0800

    task:gateway

--- demosa-gateway/src/main/java/com/demosa/GatewayApplication.java
+++ demosa-gateway/src/main/java/com/demosa/GatewayApplication.java
@@ -2,6 +2,7 @@ package com.demosa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 描述 :
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 时间 : 2020/12/25 11:30 上午
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
